c1a5ae96327774319299e7fea8dcfbae892f7567
ViniJordao <viniciusjordao02@gmail.com>
2021-06-28 20:13:25 -0300

    Commit

---
+++ E-AgendaDB/dbo/Tables/TBCompromisso.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TBCompromisso] (
+    [Id]              INT          NOT NULL,
+    [Assunto]         VARCHAR (50) NULL,
+    [Local]           VARCHAR (50) NULL,
+    [DataCompromisso] DATETIME     NULL,
+    [HoraInicio]      DATETIME     NULL,
+    [HoraTermino]     DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
